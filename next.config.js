837c411174484480f9f5fe72197b50ec7c64006b
eilla1 <git@ella.cx>
2021-11-20 23:03:21 -0800

    Add French translation

--- next.config.js
+++ next.config.js
@@ -2,12 +2,17 @@ const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'mdx'],
   i18n: {
-    locales: ['en', 'es'],
+    locales: ['en', 'es', 'fr'],
     defaultLocale: 'en',
     domains: [
       {
         domain: 'hackclub.es',
         defaultLocale: 'es',
+        http: true
+      },
+      {
+        domain: 'fr.hackclub.com',
+        defaultLocale: 'fr',
         http: true
       }
     ]
