ff76689147a647e7e51932b4efb20d4188f45f1a
eilla1 <git@ella.cx>
2021-11-21 00:11:04 -0800

    Add Chinese translation

--- next.config.js
+++ next.config.js
@@ -2,12 +2,17 @@ const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'mdx'],
   i18n: {
-    locales: ['en', 'es'],
+    locales: ['en', 'es', 'zh'],
     defaultLocale: 'en',
     domains: [
       {
         domain: 'hackclub.es',
         defaultLocale: 'es',
+        http: true
+      },
+      {
+        domain: 'zh.hackclub.com',
+        defaultLocale: 'zh',
         http: true
       }
     ]
