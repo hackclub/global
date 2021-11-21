021a0da5395161d67d8da283ec6b39a98ceabedf
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-11-21 11:51:06 +0800

    Update _middleware.js

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,7 +1,11 @@
 import { NextResponse } from 'next/server'
 
 export function middleware(request) {
-  return request.nextUrl.locale != 'en'
-    ? NextResponse.rewrite(`/copy/${request.nextUrl.locale}`)
-    : NextResponse.redirect(`https://hackclub.com`)
+  return new Response(JSON.stringify({ locale: request.nextUrl.locale }), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+  // return request.nextUrl.locale != 'en'
+  //  ? NextResponse.rewrite(`/copy/${request.nextUrl.locale}`)
+  //  : NextResponse.redirect(`https://hackclub.com`)
 }
