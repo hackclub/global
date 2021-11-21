a48f2e8a74ea77f5eda16328dd9cd8a0d2caec9b
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-11-21 11:57:20 +0800

    Update _middleware.js

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 export function middleware(request) {
   if (!request.nextUrl.pathname.includes('copy')) {
     return request.nextUrl.locale != 'en'
-      ? NextResponse.rewrite(`/copy/${request.nextUrl.locale}`)
+      ? NextResponse.rewrite(`https://global.hackclub.dev/copy/${request.nextUrl.locale}`)
       : NextResponse.redirect(`https://hackclub.com`)
   } else {
     return null
